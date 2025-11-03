cf725cbfacdf768bd3d7a381958a73e0e797d664
Angelina <Angelina@0_0>
2025-11-03 11:43:37 +0200

    Добавьте файлы проекта.

---
+++ ghfrnbxyf91/ghfrnbxyf91.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath> 
+#include <Windows.h>
+using namespace std;
+
+int main() { 
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+    int n;
+    cout << "Введіть n: ";
+    cin >> n;
+
+    double sum = 0;
+
+    for (int k = 1; k <= n; k++) {
+        sum += pow(k, k);  
+    }
+
+    cout << "Сума pow(k,k) від 1 до " << n << " = " << sum << endl;
+
+    return 0;
+}
